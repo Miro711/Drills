b3c7f6a3534f56fa3cf97d47679634215f82add2
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-09 19:09:22 -0800

    Added cohortsRouter and connected to app.js

--- super_team_picker/app.js
+++ super_team_picker/app.js
@@ -10,8 +10,11 @@ app.get('/', (req, res) => {
     res.render('welcome');
 });
 
+const cohortsRouter = require('./routes/cohortsRouter.js');
+app.use('/cohorts', cohortsRouter);
+
 const PORT = 5000;
 const HOST = 'localhost';
 app.listen(PORT, HOST, () => {
-    console.log(`The server is listening on htt[://${HOST}:${PORT}]`);
+    console.log(`The server is listening on http://${HOST}:${PORT}]`);
 });
