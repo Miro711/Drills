38d6178485ef694429ba1927aaeca51ace5d9347
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-16 22:56:12 -0800

    Generate post model with migrations, create validations within post.rb class Post and use Faker module within seeds.rb to seed the db

---
+++ blog_on_rails/app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+
+    validates(:title, 
+        presence: true, 
+        uniqueness: { case_sensitive: false }
+    )
+
+    validates(:body, 
+    presence: true, 
+    length: { minimum: 50 }
+    )
+    
+end
--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+50.times do
+    created_at = Faker::Date.backward(365*5) # Date is class inside Faker module
+    Post.create(
+        title: Faker::Company.catch_phrase, #Hacker.say_something_smart
+        body: Faker::Lorem.paragraph,
+        created_at: created_at,
+        updated_at: created_at
+        )
+end
+
+Post = Post.all
+puts Cowsay.say("Generated #{Post.count} posts", :frogs)
