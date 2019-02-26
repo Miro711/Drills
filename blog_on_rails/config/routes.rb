0c7ae44cb2231c99234ae0c8117553bb85216ab7
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-25 22:32:08 -0800

    Generate Sessions controller, define 3 resource session routes with new, create & destroy actions and and draft new view template for Sign In form to create session

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [ :new, :create]
+  resource :session, only: [ :new, :create, :destroy]
   get("/", to: "posts#index", as: :root)
   resources :posts do
     resources :comments, only: [:create, :destroy]
