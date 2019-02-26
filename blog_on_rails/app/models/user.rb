b3365e510906663b5b04089114f59ddc933127c9
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-02-25 23:28:40 -0800

    Define User instance method full_name to display user names besides created posts & comments in show page

--- blog_on_rails/app/models/user.rb
+++ blog_on_rails/app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true,
     format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    def full_name
+        "#{name}".strip
+    end
 end
