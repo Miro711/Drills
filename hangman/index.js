19ca036922dc97b273176c73757fec533504ad21
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-06 23:14:46 -0800

    Build a grid of clickable letters

--- hangman/index.js
+++ hangman/index.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    
-  
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (let i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+    }
+
+    $('#alphabet li#letter').on('click', function(event) {
+        console.log($(this));
+        $(this).addClass('highlighted');
+    });
+
+
 });
