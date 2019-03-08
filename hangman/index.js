5bd44fe14999b2fa09710b6f42a69ee406b47de5
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-03-07 23:11:42 -0800

    Implement hangman game logic displaying correct guessed letter, drawing more body parts with incorrect guesses and ending game after guessing word correctly or running out of guesses

--- hangman/index.js
+++ hangman/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 
+    let word = 'podiatrist';
+    let wrongGuesses = 0;
+    let guessedWord = [];
     const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
 
     myButtons = document.getElementById('buttons');
     letters = document.createElement('ul');
@@ -16,10 +20,40 @@ $(document).ready(function () {
         letters.appendChild(list);
     }
 
-    $('#alphabet li#letter').on('click', function(event) {
-        console.log($(this));
-        $(this).addClass('highlighted');
-    });
+    wordHolder = document.getElementById('word');
+    correct = document.createElement('ul');
 
+    for (let i = 0; i < word.length; i++) {
+        correct.id = 'my-word';
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        guess.innerHTML = "_";
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+    }
+
+    $('#alphabet li#letter').on('click', function (event) {
+        $(this).addClass('highlighted');
+        const guessedLetter = $(this).html().toLowerCase();
+        for (let char = 0; char <= word.length - 1; char++) {
+            if (word[char] == guessedLetter) {
+                $('li.guess').eq(char).html(`${guessedLetter.toUpperCase()}`);
+                guessedWord[char] = guessedLetter;
+                console.log(guessedWord);
+                if (guessedWord.join("") == word) {
+                    setTimeout(function(){ alert("Congratulations! You win!"); }, 100);
+                }
+            }
+        }
+
+        if (word.indexOf(guessedLetter) == -1) {
+            wrongGuesses += 1;
+            $('div#lives img').attr("src", `images/${wrongGuesses}.jpg`);
+            if (wrongGuesses == 6) {
+                setTimeout(function(){ alert("Better luck next time..."); }, 100);
+            }
+        }
+
+    });
 
 });
